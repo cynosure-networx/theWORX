90609677d043108dc6a05e62d48491a7e563aba1
Paul Willridge <Paul@codeworxlabs.com>
2024-12-17 16:29:22 -0500

    setup telescope for stgaging

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -61,4 +61,19 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
             ]);
         });
     }
+
+    /**
+ * Configure the Telescope authorization services.
+ *
+ * @return void
+ */
+ protected function authorization()
+ {
+    $this->gate();
+
+    Telescope::auth(function ($request) {
+        return app()->environment(['local', 'staging']) ||
+        Gate::check('viewTelescope', [$request->user()]);
+    });
+ }
 }
