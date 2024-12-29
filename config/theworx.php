fecc65c1282c019cf0a2553cef6db8407b1eb2e6
Paul Willridge <Paul@codeworxlabs.com>
2024-12-29 02:40:50 -0500

    added config theworx file, updated .env.example and Welcome.vue

--- config/theworx.php
+++ config/theworx.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
+<<<<<<< HEAD
     'company' => 'CodeWorx Labs',
 ];
+=======
+    'company' => 'CodeWorxLabs',
+    'website-name' => 'theWORX',
+    'copyright-start' => '2019',
+    'copyright-year' => date('Y'),
+    'initial-user-username' => 'JohnDoe',
+    'initial-user-password' => 'password',
+    'initial-user-first-name' => 'John',
+    'initial-user-last-name' => 'Doe',
+    'initial-user-email' => 'john.doe@codeworxlabs.com',
+];
+>>>>>>> 4b6ceb2 (added config theworx file, updated .env.example and Welcome.vue)
